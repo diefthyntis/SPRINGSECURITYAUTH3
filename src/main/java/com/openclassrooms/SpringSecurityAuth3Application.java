c2d7a43abd314fd891e6162e6d9b2b314c7a31d5
Alan Hauser <diefthyntis@gmail.com>
2024-07-20 17:33:00 +0200

    Ajout de 2 users dont un appelé lostone

---
+++ src/main/java/com/openclassrooms/SpringSecurityAuth3Application.java
@@ -0,0 +1,13 @@
+package com.openclassrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuth3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuth3Application.class, args);
+	}
+
+}
